a8fe7ed79d2ac8b36087e48e966f2ca654a909c2
Brandon-51 <bca5armstrong@gmail.com>
2022-10-06 13:48:54 -0600

    00, 01, 02

--- problems/01-arrow-full-name.js
+++ problems/01-arrow-full-name.js
@@ -7,15 +7,22 @@ keyword will allow any value assigned to that variable protection from being
 reassigned within that scope.
 
 Examples:
-let p1 = {firstName: 'John', lastName: 'Doe'};
-arrowGetFullName(p1); // => 'John Doe'
-let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
-arrowGetFullName(p2); // => 'Charlie Brown'
+
 
 ***********************************************************************/
 
 // Your code here
 
+const arrowGetFullName = (person) => {
+  let name1 = person["firstName"];
+  let name2 = person["lastName"];
+  return name1 + " " + name2;
+};
+
+let p1 = { firstName: "John", lastName: "Doe" };
+arrowGetFullName(p1); // => 'John Doe'
+let p2 = { firstName: "Charlie", lastName: "Brown", age: 9 };
+arrowGetFullName(p2); // => 'Charlie Brown'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
